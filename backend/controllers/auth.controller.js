10a0ef6c5b88c1fa3a9b6fa3cbf4b84d54125a9e
Tushar <tushar.nyc20@gmail.com>
2025-10-06 21:47:28 +0530

    verify-email endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -43,6 +43,27 @@ export const signup = async (req, res) => {
     }
 }
 
+export const verifyEmail = async (req, res) => {
+    const {code} = req.body;
+    try{
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationTokenExpiresAt: {$gt: Date.now()},
+        })
+        if(!user) {
+            return res.status(400).json({message: "Invalid or expired verification code"});
+        }
+
+        user.isVerified = true;;
+        user.verificationToken = undefined;
+        user.verificationTokenExpiresAt = undefined;
+        await user.save();
+
+    }catch(error) {
+        return res.status(500).json({message: error.message});
+    }
+}
+
 export const login = async (req, res) => {
     res.send("Login Controller");
 }
