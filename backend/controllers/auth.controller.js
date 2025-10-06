361bb652f6c3171c27eb583d92732e5a7ef21666
Tushar <tushar.nyc20@gmail.com>
2025-10-06 15:23:23 +0530

    controllers initiated

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+
+export const signup = async (req, res) => {
+    res.send("Signup Controller");
+}
+
+export const login = async (req, res) => {
+    res.send("Login Controller");
+}
+
+export const logout = async (req, res) => {
+    res.send("Logout Controller");
+}
