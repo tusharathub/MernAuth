6b2ed19a3bfd454003a139441951a21421bae92b
Tushar <tushar.nyc20@gmail.com>
2025-10-06 23:50:36 +0530

    check-auth endpoint correction

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -11,6 +11,7 @@ export const verifyToken = (req, res, next) => {
         if(!decoded) return res.status(400).json({message: "Invalid token, you are unauthorized"});
 
         req.userId = decoded.userId;
+        next();
     } catch (error) {
         console.log("Error in verifying token", error);
         return res.status(500).json({success: false, message: "server error"});
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -10,6 +10,6 @@ router.post("/logout", logout);
 router.post("/verify-email", verifyEmail);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
-router.get("/check-auth", verifyToken, checkAuth);
+router.post("/check-auth", verifyToken, checkAuth);
 
 export default router;
