d2ccec638513e261d1239e83bf9e9a732dff3d7d
Tushar <tushar.nyc20@gmail.com>
2025-10-08 01:58:38 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,9 @@ app.use(cors({origin: "http://localhost:5173", credentials: true}));
 app.use("/api/auth", authRoutes);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.json(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     })
 }
