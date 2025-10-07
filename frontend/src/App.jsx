46b5ecadd38e2aaedd53846955315d4d9d559dae
Tushar <tushar.nyc20@gmail.com>
2025-10-07 13:50:33 +0530

    frontend start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-gray-200'>Welcome</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import react from 'react';
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
