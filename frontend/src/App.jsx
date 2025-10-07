9f57c8087628a7d579d6390eaf7d662bf6fc246f
Tushar <tushar.nyc20@gmail.com>
2025-10-07 14:26:26 +0530

    floating thing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
+import FloatingThing from './components/FloatingThing'
 
 function App() {
   return (
-    <>
-      <h1 className='bg-gray-200'>Welcome</h1>
-    </>
+  <div className='min-h-screen bg-gradient-to-br from-gray-600 via-blue-900 to-purple-600 flex items-center justify-center relative overflow-hidden'>
+    <h1>Welcome back</h1>
+    <FloatingThing color='bg-red-500' size="w-64 h-64" top="-5%" left="10%" delay={0} />
+    <FloatingThing color='bg-red-500' size="w-64 h-64" top="35%" left="15%" delay={0} />
+    <FloatingThing color='bg-green-900' size="w-64 h-64" top="51%" left="65%" delay={0} />
+  </div>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import react from 'react';
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
 )
