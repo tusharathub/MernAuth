88c4c4cb56773be48367b313692f3810077ff8d1
Tushar <tushar.nyc20@gmail.com>
2025-10-07 15:13:09 +0530

    frontend sighup page base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import LogInPage from './pages/LogInPage';
 function App() {
   return (
   <div className='min-h-screen bg-gradient-to-br from-gray-600 via-blue-900 to-purple-600 flex items-center justify-center relative overflow-hidden'>
-    <h1>Welcome back</h1>
     <FloatingThing color='bg-red-500' size="w-64 h-64" top="-5%" left="10%" delay={0} />
     <FloatingThing color='bg-red-500' size="w-64 h-64" top="35%" left="15%" delay={0} />
     <FloatingThing color='bg-green-900' size="w-64 h-64" top="51%" left="65%" delay={0} />
