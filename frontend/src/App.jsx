1f667fa6ecdfda6e7052d55f312d1232e99d9ac3
Tushar <tushar.nyc20@gmail.com>
2025-10-07 14:43:27 +0530

    frontend route setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
 import FloatingThing from './components/FloatingThing'
+import {Route, Routes} from "react-router-dom";
+import SignupPage from './pages/SignupPage';
+import LogInPage from './pages/LogInPage';
 
 function App() {
   return (
@@ -8,7 +10,14 @@ function App() {
     <FloatingThing color='bg-red-500' size="w-64 h-64" top="-5%" left="10%" delay={0} />
     <FloatingThing color='bg-red-500' size="w-64 h-64" top="35%" left="15%" delay={0} />
     <FloatingThing color='bg-green-900' size="w-64 h-64" top="51%" left="65%" delay={0} />
-  </div>
+
+    <Routes>
+
+      <Route path="/" element={"Home"} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/login" element={<LogInPage />} />
+    </Routes>
+  </div>  
   )
 }
 
