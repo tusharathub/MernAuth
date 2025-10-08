901cfb41113913b906116f154a52685e08020003
Tushar <tushar.nyc20@gmail.com>
2025-10-08 12:09:25 +0530

    final fr

--- backend/index.js
+++ backend/index.js
@@ -5,33 +5,25 @@ import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import path from "path";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const rootPath = path.resolve(__dirname, "", "");
-
-dotenv.config({ path: path.join(rootPath, ".env")});
-
+dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
 
-const projectedRoute = path.resolve(__dirname, "", "", "");
+const __dirname = path.resolve();
 
 app.use(cors({origin: "http://localhost:5173", credentials: true}));
 
 app.use("/api/auth", authRoutes);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(projectedRoute, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
     app.get(/.*/, (req, res) => {
-        res.sendFile(path.resolve(projectedRoute, "frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     })
 }
 
